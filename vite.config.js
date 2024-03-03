14b3b201fcba0228827443f8819ac60085e5cf0c
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2024-03-03 21:29:35 +0200

    updates to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+/* eslint-disable no-undef */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+const { VITE_FORM_KEY } = process.env;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  define: {
+    "import.meta.env.VITE_FORM_KEY": VITE_FORM_KEY,
+  },
 });
