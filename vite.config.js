af0004cf194c03035072d2cf4d5f293c2bd81561
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2024-03-03 21:37:47 +0200

    reverts some changes

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,7 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  define: {
-    "import.meta.env.VITE_FORM_KEY": process.env.VITE_FORM_KEY,
-  },
 });
