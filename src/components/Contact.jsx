0b7c0aaded5c5cb6386e6725850a8b193261452d
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2024-03-03 22:04:22 +0200

    running logs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ import "../styles/Homepage.css";
 
 const Contact = () => {
   const formSpreeKey = import.meta.env.VITE_FORM_KEY;
-  console.log(formSpreeKey);
+  console.log(String(formSpreeKey));
   const [state, handleSubmit] = useForm(formSpreeKey);
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const [formData, setFormData] = useState({
