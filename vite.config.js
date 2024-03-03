fff8a54ca6c2bc43deded2e3011c654476bfe87f
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2024-03-03 21:33:27 +0200

    deeebugging

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const { VITE_FORM_KEY } = process.env;
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/",
   define: {
-    "import.meta.env.VITE_FORM_KEY": VITE_FORM_KEY,
+    "import.meta.env.VITE_FORM_KEY": process.env.VITE_FORM_KEY,
   },
 });
